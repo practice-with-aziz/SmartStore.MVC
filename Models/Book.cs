6ec323c66e08db1386ac4e3f0a14871d224f41e6
Zishan Ansari <rs.azizansari@gmail.com>
2026-05-03 15:46:11 +0530

    feat: add Book model, DbContext and Initial migration

---
+++ Models/Book.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SmartStore.MVC.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(100)]
+        public string Author { get; set; } = string.Empty;
+
+        [Range(0.01,10000)]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+        public Genre Genre { get; set; }
+
+    }
+
+    public enum Genre
+    {
+        Fiction,
+        NonFiction,
+        Mystery,
+        Thriller,
+        ScienceFiction,
+        Fantasy,
+        Romance,
+        Horror,
+        HistoricalFiction,
+        Biography,
+        Autobiography,
+        SelfHelp,
+        Business,
+        Programming,
+        Technology,
+        Philosophy,
+        Psychology,
+        Science,
+        History,
+        Poetry,
+        Drama,
+        Adventure,
+        YoungAdult,
+        ChildrensComics
+    }
+}
+
