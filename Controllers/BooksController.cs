f2f42df2878dafb8251567a26e7856f4b25f413a
Zishan Ansari <rs.azizansari@gmail.com>
2026-05-03 16:22:01 +0530

    feat: add BooksController, Index action and Index view

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmartStore.MVC.Data;
+using SmartStore.MVC.Models;
+
+namespace SmartStore.MVC.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly AppDbContext _context;
+        public BooksController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var books = await _context.Books.ToListAsync();
+            return View(books);
+        }
+    }
+}
---
+++ Views/Books/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SmartStore.MVC.Views.Books
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
